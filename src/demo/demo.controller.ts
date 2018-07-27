67ee12477eb8d97f456a51b7059b10ba27e5ebab
wanghao8080 <117663444@qq.com>
2018-07-27 11:16:04 +0800

    路由参数

--- src/demo/demo.controller.ts
+++ src/demo/demo.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('demo')
 export class DemoController {
   @Get()
   ghostStory() {
     return '😄 😅 😨 😱 🦖 👻';
+  }
+
+  @Get(':id')
+  show(@Param('id') id) {
+    return `id: ${ id }`;
   }
 }
