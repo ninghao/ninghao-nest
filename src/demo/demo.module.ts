bf10331a9613332e89dfd7f95e0299a0f74a09d2
wanghao8080 <117663444@qq.com>
2018-07-27 13:51:33 +0800

    中间件：Middleware

--- src/demo/demo.module.ts
+++ src/demo/demo.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { DemoController } from './demo.controller';
 import { DemoService } from './demo.service';
+import { DemoMiddleware } from './demo.middleware';
 
 @Module({
   controllers: [DemoController],
   providers: [DemoService],
 })
-export class DemoModule {}
+export class DemoModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(DemoMiddleware)
+      .forRoutes(DemoController);
+  }
+}
